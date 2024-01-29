09cb7284008a35bd7d2b052ee1e52b702fcb6f72
Fabirc98 <frcorte@sudamericano.edu.ec>
2024-01-28 20:21:39 -0500

    .html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WComponent } from './components/w/w.component';
+import { FComponent } from './components/f/f.component';
+import { ZComponent } from './components/z/z.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,WComponent,FComponent,ZComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
